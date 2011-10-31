25842b4904149fee4965e5877c6c80f8f194cca3
JoeDralliam <jdralliam@gmail.com>
2011-10-31 11:39:16 +0100

    SoundStream implemented

--- Audio/audio_stub.cpp
+++ Audio/audio_stub.cpp
@@ -21,13 +21,53 @@ extern "C"
 							&sf::Listener::GetDirection )
 }
 
+custom_enum_affectation( sf::SoundSource::Status );
+custom_enum_conversion( sf::SoundSource::Status );
 
+typedef sf::SoundSource sf_SoundSource;
+#define CAMLPP__CLASS_NAME() sf_SoundSource
+camlpp__register_custom_class()
+	camlpp__register_constructor1( copy_constructor, sf::SoundSource const& )
+	camlpp__register_method1( SetPitch, &sf::SoundSource::SetPitch )
+	camlpp__register_method1( SetVolume, &sf::SoundSource::SetVolume )
+	camlpp__register_method3( SetPosition,
+				  ((void (sf::SoundSource::*)(float, float, float)) &sf::SoundSource::SetPosition) )
+	camlpp__register_method1( SetPositionV,
+				  ((void (sf::SoundSource::*)(sf::Vector3f const&)) &sf::SoundSource::SetPosition) )
+	camlpp__register_method1( SetRelativeToListener, &sf::SoundSource::SetRelativeToListener )
+	camlpp__register_method1( SetMinDistance, &sf::SoundSource::SetMinDistance )
+	camlpp__register_method1( SetAttenuation, &sf::SoundSource::SetAttenuation )
+	camlpp__register_method0( GetPitch, &sf::SoundSource::GetPitch )
+	camlpp__register_method0( GetVolume, &sf::SoundSource::GetVolume )
+	camlpp__register_method0( GetPosition, &sf::SoundSource::GetPosition )
+	camlpp__register_method0( IsRelativeToListener, &sf::SoundSource::IsRelativeToListener )
+	camlpp__register_method0( GetMinDistance, &sf::SoundSource::GetMinDistance )
+	camlpp__register_method0( GetAttenuation, &sf::SoundSource::GetAttenuation )
+camlpp__custom_class_registered()
+#undef CAMLPP__CLASS_NAME
 
+typedef sf::SoundStream sf_SoundStream;
+#define CAMLPP__CLASS_NAME() sf_SoundStream
+camlpp__register_custom_class()
+	camlpp__register_inheritance_relationship( sf_SoundSource )
+	camlpp__register_method0( Play, &sf::SoundStream::Play )
+	camlpp__register_method0( Pause, &sf::SoundStream::Pause )
+	camlpp__register_method0( Stop, &sf::SoundStream::Stop )
+	camlpp__register_method0( GetChannelsCount, &sf::SoundStream::GetChannelsCount )
+	camlpp__register_method0( GetSampleRate, &sf::SoundStream::GetSampleRate )
+	camlpp__register_method0( GetStatus, &sf::SoundStream::GetStatus )
+	camlpp__register_method1( SetPlayingOffset, &sf::SoundStream::SetPlayingOffset )
+	camlpp__register_method0( GetPlayingOffset, &sf::SoundStream::GetPlayingOffset )
+	camlpp__register_method1( SetLoop, &sf::SoundStream::SetLoop )
+	camlpp__register_method0( GetLopp, &sf::SoundStream::GetLoop )
+camlpp__custom_class_registered()
+#undef CAMLPP__CLASS_NAME
 
 
 typedef sf::Music sf_Music;
 #define CAMLPP__CLASS_NAME() sf_Music
 camlpp__register_custom_class()
+	camlpp__register_inheritance_relationship( sf_SoundStream )
 	camlpp__register_constructor0( default_constructor )
 	camlpp__register_method1( OpenFromFile, &sf::Music::OpenFromFile )
 //	camlpp__register_method1( OpenFromMemory
@@ -98,34 +138,6 @@ typedef sf::SoundBufferRecorder sf_SoundBufferRecorder;
 camlpp__register_custom_class()
 	camlpp__register_inheritance_relationship( sf_SoundRecorder )
 	camlpp__register_method0( GetBuffer, &sf::SoundBufferRecorder::GetBuffer );
-camlpp__custom_class_registered()
-#undef CAMLPP__CLASS_NAME
-
-
-
-
-custom_enum_affectation( sf::SoundSource::Status );
-custom_enum_conversion( sf::SoundSource::Status );
-
-typedef sf::SoundSource sf_SoundSource;
-#define CAMLPP__CLASS_NAME() sf_SoundSource
-camlpp__register_custom_class()
-	camlpp__register_constructor1( copy_constructor, sf::SoundSource const& )
-	camlpp__register_method1( SetPitch, &sf::SoundSource::SetPitch )
-	camlpp__register_method1( SetVolume, &sf::SoundSource::SetVolume )
-	camlpp__register_method3( SetPosition,
-				  ((void (sf::SoundSource::*)(float, float, float)) &sf::SoundSource::SetPosition) )
-	camlpp__register_method1( SetPositionV,
-				  ((void (sf::SoundSource::*)(sf::Vector3f const&)) &sf::SoundSource::SetPosition) )
-	camlpp__register_method1( SetRelativeToListener, &sf::SoundSource::SetRelativeToListener )
-	camlpp__register_method1( SetMinDistance, &sf::SoundSource::SetMinDistance )
-	camlpp__register_method1( SetAttenuation, &sf::SoundSource::SetAttenuation )
-	camlpp__register_method0( GetPitch, &sf::SoundSource::GetPitch )
-	camlpp__register_method0( GetVolume, &sf::SoundSource::GetVolume )
-	camlpp__register_method0( GetPosition, &sf::SoundSource::GetPosition )
-	camlpp__register_method0( IsRelativeToListener, &sf::SoundSource::IsRelativeToListener )
-	camlpp__register_method0( GetMinDistance, &sf::SoundSource::GetMinDistance )
-	camlpp__register_method0( GetAttenuation, &sf::SoundSource::GetAttenuation )
 camlpp__custom_class_registered()
 #undef CAMLPP__CLASS_NAME
 
