a8645a8ef8848ed836955b4a993ac11950ffafb6
kyoDralliam <chocobodralliam@gmail.com>
2011-10-31 14:40:54 +0100

    audio avancé

--- Audio/audio.ml
+++ Audio/audio.ml
@@ -12,14 +12,54 @@ struct
 
 end
 
+module SoundSource =
+struct
+  type status = Stopped | Paused | playing
+end
+
+external class virtual sound_source : "sf_SoundSource" =
+object auto (_:'a)
+  constructor copy : 'a -> 'a = "copy_constructor"
+  external method set_pitch : float -> unit = "SetPitch"
+  external method set_volume : float -> unit = "SetVolume"
+  external method set_position : float -> float -> float -> unit = "SetPosition"
+  external method set_position_v : float * float * float -> unit = "SetPositionV"
+  external method set_relative_to_listener : bool -> unit = "SetRelativeListener" 
+  external method set_min_distance : float -> unit = "SetMinDistance"
+  external method set_attenuation : float -> unit = "SetAttenuation"
+  external method get_pitch : unit -> float = "GetPitch"
+  external method get_volume : unit -> float  = "GetVolume"
+  external method get_position : unit -> float * float * float = "GetPosition"
+  external method is_relative_to_listener : unit -> bool = "IsRelativeToListener"	     
+  external method get_min_distance : unit -> float = "GetMinDistance"
+  external method get_attenuation : unit -> float = "GetAttenuation"
+end
+
+
+external class virtual sound_stream : "sf_SoundStream" =
+object 
+  external inherit sound_source : "sf_SoundSource"
+  external method play : unit -> unit  = "Play"
+  external method pause : unit -> unit = "Pause"
+  external method stop : unit -> unit = "Stop"
+  external method get_channel_count : unit -> int = "GetChannelCount"
+  external method get_sample_rate : unit -> int = "GetSampleRate"
+  external method get_status : unit -> SoundSource.status = "GetStatus"	     
+  external method set_playing_offset : int -> unit = "SetPlayingOffset"
+  external method get_playing_offset : unit -> int = "GetPlayingOffset"
+  external method set_loop : bool -> unit = "SetLoop"
+  external method get_loop : unit -> bool = "GetLoop"
+end
 
 external class musicCpp (Music) : "sf_Music" =
 object
+  external inherit sound_stream : "sf_SoundStream"
   constructor default : unit = "default_constructor"
   external method open_from_file : string -> bool = "OpenFromFile"
   external method get_duration : unit -> int = "GetDuration"
-
 end
+
+class music = let t = Music.default () in musicCpp t
 
 type samples_type = (int, int_16_signed_elt, Bigarray.c_layout) Bigarray.Array1.t
 
@@ -38,11 +78,33 @@ object auto (_ : 'a)
   external method get_duration : unit -> int = "GetDuration"
 end
 
-external class sound_recorder (SoundRecorder) : "sf_SoundRecorder" =
+class sound_buffer = let t = SoundBuffer.default () in sound_bufferCpp t
+
+external class virtual sound_recorder : "sf_SoundRecorder" =
 object
   external method start : ?sampleRate:int -> unit -> unit = "Start"
   external method stop : unit -> unit = "Stop"
   external method get_sample_rate : unit -> int = "GetSampleRate"
 end
 
-external cpp is_available : unit -> bool = "SoundRecorder_IsAvailable"
+module SoundRecorder =
+struct
+  external cpp is_available : unit -> bool = "SoundRecorder_IsAvailable"
+end
+
+external class sound_buffer_recorderCpp (SoundBufferRecorder) : "sf_SoundBufferRecorder" =
+object 
+  external inherit sound_recorder : "sf_SoundRecorder"
+  external method get_buffer : unit -> sound_buffer = "GetBuffer"
+end
+
+class sound_buffer_recorder = 
+  let t = SoundBufferRecorder.default () in
+    sound_buffer_recorderCpp t
+
+
+external class soundCpp (Sound) : "sf_Sound" =
+object
+  external inherit 
+
+end
