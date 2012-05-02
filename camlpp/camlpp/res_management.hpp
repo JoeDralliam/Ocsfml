b839fcd88943b82b1f769f982d9de1628f019102
Joe Dralliam <jdralliam@gmail.com>
2012-05-02 12:19:54 +0200

    wip

--- camlpp/camlpp/res_management.hpp
+++ camlpp/camlpp/res_management.hpp
@@ -54,7 +54,7 @@ namespace camlpp
   {
   private:
     template<class Func, class... Args>
-    std::result_of<Func(Args...)>::type call_helper(Func&& f, Args&&... args)
+    typename std::result_of<Func(Args...)>::type call_helper(Func&& f, Args&&... args)
     {
       scoped_release_rt runtime_released;
       return f(std::forward<Args>(args)...);
